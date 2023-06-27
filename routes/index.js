a758a9f33a9715cdc69ddce3c3fcf554b1ae4950
nleung77 <nleung77@gmail.com>
2023-06-26 20:28:12 -0400

    updated files

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import {Router} from "express";
+//import routes from controllers
+
+const router = Router()
+
+router.get("/", (req, res) => res.send("This is the api root!"));
+
+// ex. router.use("/route", ____Routes)
+router.use("/", );
+router.use("/",);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,28 @@
+import db from "./db/connection.js";
+import routes from "./routes/index.js"
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+import chalk from "chalk";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
+
+app.use("/", routes)
+
+db.on("connected", () => {
+  console.clear();
+  console.log(chalk.blue("Connected to MONGODB!"));
+
+  app.listen(PORT, () => {
+    process.env.NODE_ENV === "production"
+      ? console.log(`Express server running in production on port ${PORT}\n\n`)
+      : console.log(`Express server running in development on: ${PORT}`);
+  });
+});
+
+
